3c973a67406a688eff876190d04c605fa24c1af2
headlulu888 <headlulu888@gmail.com>
2019-06-08 23:10:58 +0300

    add frontend framework && use on form

--- src/Form/TestFormType.php
+++ src/Form/TestFormType.php
@@ -6,6 +6,7 @@ namespace App\Form;
 use App\Entity\Page;
 use function Sodium\add;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,13 +20,26 @@ class TestFormType extends AbstractType
         $builder
 //            ->add('title', 'Symfony\Component\Form\Extension\Core\Type\TextareaType')
             ->add('title', TextareaType::class, [
-                'label' => 'Заголовок'
+                'label_attr' => ['class' => 'label'],
+                'label' => 'Заголовок',
+                'attr' => [
+                    'class' => 'input'
+                ]
             ])
             ->add('content', TextareaType::class, [
                 'label' => 'Контент',
-                'label_attr' => ['class' => 'random-class']
+                'label_attr' => ['class' => 'label'],
+                'attr' => [
+                    'class' => 'textarea'
+                ]
             ])
-            ->add('publish');
+            ->add('publish', CheckboxType::class, [
+                'label' => 'Контент',
+                'label_attr' => ['class' => 'label'],
+                'attr' => [
+                    'class' => 'checkbox'
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)

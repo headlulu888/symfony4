0d3ee7ff54ebbcc64ad865141246dd09eddb2c6a
headlulu888 <headlulu888@gmail.com>
2019-06-08 22:40:49 +0300

    add nullable value for fields

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -17,12 +17,12 @@ class Page
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $title;
 
     /**
-     * @ORM\Column(type="text")
+     * @ORM\Column(type="text", nullable=true)
      */
     private $content;
 
